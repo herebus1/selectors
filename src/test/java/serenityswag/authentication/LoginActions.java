5b54cf3ee8ccd675a570ae9896980b879e8d1c7b
Jairo Martinez <jairo.martinez@loadsure.net>
2023-01-24 19:18:33 -0500

    Add methods to select the elements on the page

--- src/test/java/serenityswag/authentication/LoginActions.java
+++ src/test/java/serenityswag/authentication/LoginActions.java
@@ -2,6 +2,7 @@ package serenityswag.authentication;
 
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.By;
 
 public class LoginActions extends UIInteractionSteps {
     @Step("Log in as {0}")
@@ -9,9 +10,15 @@ public class LoginActions extends UIInteractionSteps {
         openUrl("https://www.saucedemo.com/");
 
         // Login as a standard user
-        $("[data-test='username']").sendKeys(user.getUsername());
-        $("[data-test='password']").sendKeys(user.getPassword());
-        $("[data-test='login-button']").click();
+        //$("[data-test='username']").sendKeys(user.getUsername());
+        //find(By.id("user-name")).sendKeys(user.getUsername());
+        $("#user-name").sendKeys(user.getUsername());
+        //$("[data-test='password']").sendKeys(user.getPassword());
+        //find(By.id("password")).sendKeys(user.getPassword());
+        $("#password").sendKeys(user.getPassword());
+        //$("[data-test='login-button']").click();
+        //find(By.id("login-button")).click();
+        $("#login-button").click();
     }
 
 }
